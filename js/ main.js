91fd83e4642dcfd84521f3f4b04c0b66e8b3e8e5
erynjones <eryn.jones16@hotmail.com>
2016-03-01 13:54:11 -0500

    done html and started other changes

--- js/ main.js
+++ js/ main.js
@@ -1,6 +1,6 @@
-var hiddenMessage = '<p><span>\u2606</span>\x53\x75\x70\x65\x72\x73\x74\x61\x72\x21</p>;
+var hiddenMessage = prompt('\u2606' + '\x53\x75\x70\x65\x72\x73\x74\x61\x72\x21');
 var i = 0;
 
-for (i = 0; i < 10 i++) {
+for (i = 0; i < 10; i++) {
   document.write(hiddenMessage);
-
+}
